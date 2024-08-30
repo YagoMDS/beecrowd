9559277e29224c3a71b5577dd570406f4d711e7c
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2024-08-30 08:47:30 -0300

    Update 1021.py
    Arrumando Erro

---
+++ iniciante/1021.py
@@ -0,0 +1,32 @@
+def calcular_notas(valor):
+    # Notas disponíveis
+    notas = [100, 50, 20, 10, 5, 2, 1]
+    quantidade_notas = {}
+
+    # Calcula a quantidade de cada nota
+    for nota in notas:
+        quantidade_notas[nota] = valor // nota
+        valor %= nota
+
+    return quantidade_notas
+
+def calcular_moedas(valor):
+
+
+def main():
+    # Leitura do valor
+    valor = float(input())
+
+    # Calcula as notas necessárias
+    quantidade_notas = calcular_notas(valor)
+
+    # Impressão do valor lido
+    print(valor)
+
+    # Impressão da quantidade de cada nota
+    for nota in [100, 50, 20, 10, 5, 2, 1]:
+        print("NOTAS:")
+        print(f"{quantidade_notas[nota]} nota(s) de R$ {nota},00")
+
+if __name__ == "__main__":
+    main()
