abf65d6443b121d071f3b5f3009d3029d6beb5fc
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2024-08-30 10:58:07 -0300

    Update 1021.py

--- iniciante/1021.py
+++ iniciante/1021.py
@@ -10,19 +10,34 @@ def calcular_notas(valor):
 
     return quantidade_notas
 
+def calcular_moedas(valor):
+    moedas = [1.0, 0.50, 0.25, 0.10, 0.05, 0.01]
+    for moeda in moedas:
+        quantidade_moedas = {}
+        quantidade_moedas[moeda] = valor // moeda
+        valor %= moeda
+
+    return quantidade_moedas
 def main():
     # Leitura do valor
-    valor = int(input())
+    valor = float(input())
 
     # Calcula as notas necessárias
     quantidade_notas = calcular_notas(valor)
-
     # Impressão do valor lido
     print(valor)
+
+    quantidade_moedas = calcular_moedas(valor)
+    print(valor)
+    
+    
 
     # Impressão da quantidade de cada nota
     for nota in [100, 50, 20, 10, 5, 2, 1]:
         print(f"{quantidade_notas[nota]} nota(s) de R$ {nota},00")
+
+    for moeda in [1.0, 0.50, 0.25, 0.10, 0.05, 0.01]:
+        print(f"{quantidade_moedas[moeda]} nota(s) de R$ {moeda}")
 
 if __name__ == "__main__":
     main()
