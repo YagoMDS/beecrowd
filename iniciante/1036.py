d9b96ae902ea83d5173f48c801716a861dfaa73c
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2024-09-05 13:24:11 -0300

    Update 1036.py

--- iniciante/1036.py
+++ iniciante/1036.py
@@ -1,15 +1,18 @@
 import math 
 
 def main():
+    a, b, c = input().split()
 
-    a = float(input())
-    b = float(input())
-    c = float(input())
+    a = float(a)
+    b = float(b)
+    c = float(c)
     
     delta = (b*b) - (4*a*(c))
+    
+    if delta >=0
     raizdelta = math.sqrt(delta)
 
-    if raizdelta >= 0 and a > 00:
+    if raizdelta >= 0 and a > 00 or delta <= 0:
         x1 = (-b + (raizdelta)) / (2*a)
         x2 = (-b - (raizdelta)) / (2*a)
         print(delta)
