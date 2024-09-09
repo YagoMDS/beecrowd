4372bc9a27cca18926f8a674b1be4c0b66a253d7
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2024-09-09 14:31:20 -0300

    Update 1043.py

--- iniciante/1043.py
+++ iniciante/1043.py
@@ -1,11 +1,8 @@
 # Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
 
-
 # Perimetro = XX.X
 
-
 # Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
-
 
 # Area = XX.X
 
@@ -17,17 +14,23 @@
 
 def main():
 
-    a, b, c = input().split()
-    a = float(a)
-    b = float(b)
-    c = float(c)
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
 
-    if ((a + b > c) and (a + c > b) and (b + c > a)):
-        perimetro = a + b + c
-        print(f"Perimentro = {perimetro:.1f}")
+    if (a > b):
+        resto = a % b
+        if(resto == 0): 
+            print("Sao Multiplos")
+        else:
+            print("Nao sao Multiplos")
+    elif (b > a):
+        resto = b % a
+        if(resto == 0): 
+            print("Sao Multiplos")
+        else:
+            print("Nao sao Multiplos")
     else:
-        area = ((a + b) * c) / 2
-        print(f"Área: {area:.1f}")
-
+        print("Nao sao Multiplos")
 main()
 
