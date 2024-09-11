79d610a67becc873bcf9b122d4f24e1981bc3d31
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2024-09-11 08:40:47 -0300

    Create 1049.py

---
+++ iniciante/1049.py
@@ -0,0 +1,40 @@
+# Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+
+# Entrada
+# A entrada contém 3 palavras, uma em cada linha, necessárias para identificar o animal segundo a figura acima, com todas as letras minúsculas.
+
+# Saída
+# Imprima o nome do animal correspondente à entrada fornecida.
+
+
+def main():
+
+    p1 = input().split()
+    p2 = input().split()
+    p3 = input().split()
+
+    if p1 == 'vertebrado':
+        if p2 == 'ave':
+            if p3 == 'carnivoro':
+                print("aguia")
+            elif p3 == "onivoro":
+                print("pomba")
+        elif p2 =='mamifero':
+            if p3 == 'onivoro':
+                print("homem")
+            elif p3 == 'herbivoro':
+                print("vaca")
+    elif p1 == 'invertebrado':
+        if p2 == 'inseto':
+            if p3 == 'hematofago':
+                print("pulga")
+            elif p3 == "herbivoro":
+                print("lagarta")
+        elif p2=='anelideo':
+            if p3 == 'hematofago':
+                print("sanguessuga")
+            elif p3 == 'onivoro':
+                print("minhoca")
+    else:
+        return 0
+main()
