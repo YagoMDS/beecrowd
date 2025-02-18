45dfca967839d8fe2c134abc80a8c7ff8a0041ee
YagoMDS <97117366+YagoMDS@users.noreply.github.com>
2025-02-18 10:54:12 -0300

    Update in descriptions
    Updated descriptions, informing the initial problem

--- SQL/2606.sql
+++ SQL/2606.sql
@@ -1,3 +1,7 @@
+/*Quando os dados foram migrados de Banco de Dados, houve um pequeno mal-entendido por parte do antigo DBA.
+
+Seu chefe precisa que você exiba o código e o nome dos produtos, cuja categoria inicie com 'super'.*/
+
 select
     p.id,
     p.name
